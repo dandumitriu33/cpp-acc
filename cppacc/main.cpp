efd21a90f67fa934d70c9c5ca9a02a86a1680e7e
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-29 14:56:54 +0300

    Extract lesson one to function

--- cppacc/main.cpp
+++ cppacc/main.cpp
@@ -1,14 +1,23 @@
 // a small C++ program
 #include <iostream>
 
+void activateLessonOne();
+
 int main()
+{
+	activateLessonOne();
+	
+	return 0;
+}
+
+void activateLessonOne() 
 {
 	std::cout << "Hello, world!" << std::endl;
 	3 + 4; // this compiles and discards (not yields the result for future chaining)
 	std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
 	std::cout << "Name:\t\tBond\nSurname:\tJames\nCode:\t\t007\n" << std::endl;
-	
-	std::cout << 
+
+	std::cout <<
 		"// a small C++ program\n"
 		"#include <iostream>\n"
 		"\n"
@@ -18,6 +27,4 @@ int main()
 		"\treturn 0;\n"
 		"}\n"
 		<< std::endl;
-	
-	return 0;
 }
